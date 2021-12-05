1b3bff3714030407b56019db68c35fc0ca05a181
Vlad Chernoborodiuk <45739207+Marsianin007@users.noreply.github.com>
2021-12-05 04:20:21 +0100

    Хотел уточнить

--- HT_5.6.py
+++ HT_5.6.py
@@ -2,22 +2,43 @@
 #   P.S. Повинен вертатись генератор.
 #   P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній: https://docs.python.org/3/library/stdtypes.html#range
 
-def new_range(finish, start = 0, step = 1):
-    num = start
-    while num < finish and step > 0:
-        yield num
-        num += step
 
-    while num > finish and step < 0:
-        yield num
-        num += step
 
-for i in new_range(0, 10, -1):
+
+
+def new_range(*arg):
+    arg = list(arg)
+    arg = list(map(int, arg))
+    if len(arg) == 1:
+        finish = arg[0]
+        num = 0
+        step = 1
+        while num < finish and step > 0:
+            yield num
+            num += step
+
+    if len(arg) == 2:
+        finish = arg[1]
+        num = arg[0]
+        step = 1
+        while num < finish and step > 0:
+            yield num
+            num += step
+
+    if len(arg) == 3:
+        finish = arg[1]
+        num = arg[0]
+        step = arg[2]
+        while num < finish and step > 0:
+            yield num
+            num += step
+
+        while num > finish and step < 0:
+            yield num
+            num += step
+
+for i in new_range(0, 10):
     print(i)
 
-for i in new_range(5):
-    print(i)
-
-
-
-
+#for i in new_range(5):
+#    print(i)
